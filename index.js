1f76718f00ca53396768519707a0b669d09fbe98
Ryan Cherry <cherryryan@ecs.vuw.ac.nz>
2019-10-03 15:08:27 +1300

    added more data to serve

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-ControlAllow-Headers');
     // Pass to next layer of middleware
     next();
-   });
-   
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -22,7 +22,13 @@ app.use(express.static('public'))
 
 var port = process.env.PORT || 8080;
 
-var books = {0: {name: "Book 1", description: "this is a book", key: 0}};
+var books = {
+    0: { name: "Book 1", description: "Book 1 from best selling horror author jim", category: "horror", author: "Jim", key: 0 },
+    1: { name: "Book 2", description: "Book 2 is amazing", category: "comedy", author: "Mike", key: 1 },
+    2: { name: "Book 3", description: "Book 3 is the best", category: "comedy", author: "Mike", key: 2 },
+    3: { name: "Book 4", description: "Book 4 is pretty good", category: "comedy", author: "Mike", key: 3 },
+    4: { name: "Book 5", description: "Book 5", category: "teen drama", author: "Stephanie", key: 4 },
+};
 
 app.get('/books', (req, res) => {
     return res.send(books);
@@ -64,6 +70,6 @@ app.get('/books/:bookId', (req, res) => {
 //     return res.send(null);;
 // });
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('Example app listening on port 8080!');
 });
